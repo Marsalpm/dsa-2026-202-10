bebbb6d3867432fd40869482f3c1ef64b4dc9eba
martinasierra01-crypto <martina.sierra01@estudiant.upf.edu>
2026-04-09 10:19:04 +0200

    MAIN

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 
 void createaleak() {
   char *foo = malloc(20 * sizeof(char));
@@ -17,6 +18,18 @@ int main() {
 
   // uncomment and run "make v" to see how valgrind detects memory leaks
   // createaleak();
+
+
+
+  char mapname[20];
+
+  printf("Introdueix el nom del mapa (ex: xs_1, xs_2, md_1, lg_1, xl_1 or xl_1): ");
+  scanf("%s", mapname);
+
+  char filename[100];
+  strcpy(filename, "maps/"); //Construim un string "maps/"
+  strcat(filename, mapname); //"maps/mapname"
+  strcat(filename, "/houses.txt"); //Adreca final: "maps/mapname/houses.txt"
 
   return 0;
 }
